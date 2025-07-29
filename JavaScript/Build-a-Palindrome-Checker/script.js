fbe4308196686e38dd039b9f400720c7b7145743
WareZTv <78643965+WareZTv@users.noreply.github.com>
2025-07-29 22:25:42 +0200

    Add files via upload

---
+++ JavaScript/Build-a-Palindrome-Checker/script.js
@@ -0,0 +1,50 @@
+const textInput = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn");
+const resultOutput = document.getElementById("result");
+
+const preFormatText = text =>
+  text.replace(/[^a-z0-9]/gi, "").toLowerCase()
+
+const isPalindrome = text => {
+  const n = text.length;
+  if (n === 1) {
+    return true
+  }
+  let currentDigitIndex;
+  let currentDigitIndexMiror;
+  if (n % 2 === 0) {
+    currentDigitIndex = Math.floor(n/2) - 1;
+    currentDigitIndexMiror = Math.floor(n/2)
+  } else {
+    currentDigitIndex = Math.floor(n/2) -1;
+    currentDigitIndexMiror = Math.floor(n/2) + 1;
+  }
+
+  while (currentDigitIndex >= 0) {
+    if (text[currentDigitIndex] !== text[currentDigitIndexMiror]) {
+      return false
+    }
+    currentDigitIndex --;
+    currentDigitIndexMiror ++;
+  }
+  return true
+}
+
+const updateResult = () => {
+  if (textInput.value === "") {
+    alert("Please input a value")
+    return
+  }
+  
+  const preFormatedText = preFormatText(textInput.value);
+  console.log(preFormatedText);
+  if (isPalindrome(preFormatedText)) {
+    result.innerText = `${textInput.value} is a palindrome.`
+  } else {
+    result.innerText = `${textInput.value} is not a palindrome.`
+  }
+  
+}
+
+checkButton.addEventListener("click", updateResult);
+
